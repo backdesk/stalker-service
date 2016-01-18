8ba15a48e7f92e565ae7a6ad73b630436732eb74
james gardner <james@backdesk.io>
2016-01-18 11:48:08 +0000

    Removed session config

--- config/config.js
+++ config/config.js
@@ -1,12 +1,6 @@
 var config = {
     local : {
         port : 3333,
-        session : {
-            secret : 'uDnjlvqOGNzGsEEArWd9',
-            resave : false,
-            saveUninitialized : false,
-            cookie : { httpOnly: true, maxAge: 2419200000 }
-        },
         db : {
             uri : 'localhost/stalker'
         }
