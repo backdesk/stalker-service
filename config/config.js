f53617a139b2e47d288856f61694b99b59622e0e
james gardner <james@backdesk.io>
2016-01-18 13:50:03 +0000

    formatting

--- config/config.js
+++ config/config.js
@@ -1,24 +1,24 @@
 var config = {
-    local : {
-        port : 3333,
-        db : {
-            host : 'localhost',
-            name : 'stalker',
-            username : 'postgres',
-            password : ''
-        }
-    },
-    production : {
-        port : process.env.PORT,
-        db: {
-            host : process.env.DB_HOST,
-            port : process.env.DB_PORT,
-            username : process.env.DB_USERNAME,
-            password : process.env.DB_PASSWORD 
-        }  
+  local : {
+    port : 3333,
+    db : {
+      host : 'localhost',
+      name : 'stalker',
+      username : 'postgres',
+      password : ''
     }
+  },
+  production : {
+    port : process.env.PORT,
+    db: {
+      host : process.env.DB_HOST,
+      port : process.env.DB_PORT,
+      username : process.env.DB_USERNAME,
+      password : process.env.DB_PASSWORD 
+    }  
+  }
 };
 
 module.exports = function (mode) {
-    return config[mode] || config.local;
+  return config[mode] || config.local;
 }
